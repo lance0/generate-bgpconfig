2522b78ab7fdb83a7d10279b26adcf7021da5cac
dcolonhvvc <dcolon@hivelocity.net>
2020-01-30 13:35:49 -0500

    added extra logic

--- script.py
+++ script.py
@@ -5,7 +5,19 @@ from requests.auth import HTTPBasicAuth
 peeringdb_username = ''
 peeringdb_password = ''
 ASNtoMatch = '29802'
-
+try:
+    with open('config.json') as json_data_file:
+        settings = json.load(json_data_file)
+        #print(settings['peeringdb_creds'])
+        if settings['peeringdb_creds']['username']:
+            peeringdb_username = settings['peeringdb_creds']['username']
+        else: print('You do not have a peeringdb username configured! We may be unable to get contact information depending on the organizations privacy settings.')
+        if settings['peeringdb_creds']['password']: 
+            peeringdb_password = settings['peeringdb_creds']['password']
+        else: print('You do not have a peeringdb password configured! We may be unable to get contact information depending on the organizations privacy settings.')
+        if settings['ASN']: ASNtoMatch = settings['ASN']
+except Exception as ex:
+        print("Couldn't load configuration file config.json! Details: " + str(ex))
 
 def getIXInfo(id):
     """Function to connect to peeringDB and fetch IX information for a given IX
@@ -35,6 +47,7 @@ def getPeeringDB(ASN):
     HTTP_OK = 200
     pdb_url = 'https://api.peeringdb.com/api/net?asn__in=%s&depth=2' % ASN
     #print("Fetching PeeringDB info for %s" % ASN)
+    r = ''
     if peeringdb_username and peeringdb_password: 
         r = requests.get(pdb_url, auth=HTTPBasicAuth(peeringdb_username,peeringdb_password))
         print('Connecting to peeringdb with account ' + peeringdb_username)
@@ -51,38 +64,30 @@ def intersection(list1,list2):
     returnList = [value for value in list1 if value in list2]
     return returnList
 
+def check_dupes(dict1,dict2):
+    dupes = []
+    for ix in dict1:
+        for ix2 in dict2:
+            if ix['name'] == ix2['name']: dupes.append(ix)
+    return dupes
 
 ASN = input('Please enter the ASN: ')
 ASNinfo = getPeeringDB(ASN)['data'][0]
 ixs = ASNinfo['netixlan_set']
 commands = ''
-#print(ASNinfo)
 ASNdesc = ASNinfo['name'] + ' ' + ASNinfo['poc_set'][0]['email']
-#print(ASNdesc)
-try:
-    with open('config.json') as json_data_file:
-        settings = json.load(json_data_file)
-        #print(settings['peeringdb_creds'])
-        if settings['peeringdb_creds']['username']:
-            peeringdb_username = settings['peeringdb_creds']['username']
-        else: print('You do not have a peeringdb username configured! We may be unable to get contact information depending on the organizations privacy settings.')
-        if settings['peeringdb_creds']['password']: 
-            peeringdb_password = settings['peeringdb_creds']['password']
-        else: print('You do not have a peeringdb password configured! We may be unable to get contact information depending on the organizations privacy settings.')
-        if settings['ASN']: ASNtoMatch = settings['ASN']
-except Exception as ex:
-        print("Couldn't load configuration file config.json! Details: " + str(ex))
-
 MATCH_IX_LIST = getPeeringDB(ASNtoMatch)['data'][0]['netixlan_set']
-ixs_in_common = intersection(MATCH_IX_LIST,ixs)
+ixs_in_common = check_dupes(MATCH_IX_LIST,ixs)
+
 for ix in ixs_in_common:
     for IX_NAME in settings['FRIENDLY_IX_NAMES']:
         #print('IX Name: ' + IX_NAME + '\n' + 'IX Friendly Name: ' + settings['FRIENDLY_IX_NAMES'].get(FRIENDLY_NAME))
+        print('Checking for ' + ix['name'] + ', against ' + IX_NAME)
         if ix['name'] == IX_NAME:
-            #print('Found matching IX! ' + ix['name'])
+            print('Found matching IX! ' + ix['name'])
             ix['name'] = settings['FRIENDLY_IX_NAMES'].get(IX_NAME)
-#print("IX's in common: ")
-#print(ixs_in_common)
+
+
 for ix in ixs_in_common:
     #ixinfo = getIXInfo(ix['ix_id'])
     #groupname = str(ixinfo['data'][0]['name'])# + '-' + ixinfo['data'][0]['city']
