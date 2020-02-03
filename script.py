fab484fd26439ad9c47e51f9441096bb657830a0
dcolonhvvc <dcolon@hivelocity.net>
2020-02-03 18:08:48 -0500

    use jinja template for text generation remove some old code

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import json
 import os.path
 from requests.auth import HTTPBasicAuth
 from shutil import copyfile
+from jinja2 import Template
 
 peeringdb_username = ''
 peeringdb_password = ''
@@ -24,12 +25,6 @@ except Exception as ex:
         print("Couldn't load configuration file config.json! Details: " + str(ex))
 
 def getIXInfo(id):
-    """Function to connect to peeringDB and fetch IX information for a given IX
-    Args:
-        id: The ID of the IX in peeringDB's system, for example TPAIX is ID 833: https://www.peeringdb.com/ix/833
-    Returns:
-        A dict which contains the dataset for the given IX
-    """
     HTTP_OK = 200
     pdb_url = 'https://api.peeringdb.com/api/ix?id__in=%s&depth=2' % id
     #print("Fetching PeeringDB IX info for %s" % id)
@@ -42,12 +37,6 @@ def getIXInfo(id):
     return pdb_res
 
 def getPeeringDB(ASN):
-    """Function to connect to peeringDB and fetch results for a given ASN
-    Args:
-        ASN: A numeric, 32 bit number AS number.
-    Returns:
-        A dict which contains the dataset for a given ASN. Specific API docs here: https://api.peeringdb.com/apidocs/#!/net/Network_list
-    """
     HTTP_OK = 200
     pdb_url = 'https://api.peeringdb.com/api/net?asn__in=%s&depth=2' % ASN
     #print("Fetching PeeringDB info for %s" % ASN)
@@ -106,27 +95,22 @@ for ix in ixs_in_common:
 
 
 for ix in ixs_in_common:
-    #ixinfo = getIXInfo(ix['ix_id'])
-    #groupname = str(ixinfo['data'][0]['name'])# + '-' + ixinfo['data'][0]['city']
     groupname = ix['name']
     groupipv4 = ix['ipaddr4']
     groupipv6 = ix['ipaddr6']
-    #groupdesc = ixinfo['data'][0]['name_long'] + ' ' + ixinfo['data'][0]['tech_email']
-    commands += 'set protocols bgp group ' + groupname + ' neighbor ' + groupipv4 + ' mtu-discovery' + '\n'
-    commands += 'set protocols bgp group ' + groupname + ' neighbor ' + groupipv4 + ' import ' + groupname + '-ipv4-inbound' + '\n'
-    commands += 'set protocols bgp group ' + groupname + ' neighbor ' + groupipv4 + ' export ' + groupname + '-ipv4-outbound'+ '\n'
-    commands += 'set protocols bgp group ' + groupname + ' neighbor ' + groupipv4 + ' peer-as ' + str(ix['asn']) + '\n'
-    commands += 'set protocols bgp group ' + groupname + ' neighbor ' + groupipv4 + ' description "' + ASNdesc + '"\n'
-    commands += 'set protocols bgp group ' + groupname + ' neighbor ' + groupipv4 + ' family inet unicast' + '\n'
-    commands += 'set protocols bgp group ' + groupname + ' neighbor ' + groupipv6 + ' mtu-discovery' + '\n'
-    commands += 'set protocols bgp group ' + groupname + ' neighbor ' + groupipv6 + ' import ' + groupname + '-ipv6-inbound' + '\n'
-    commands += 'set protocols bgp group ' + groupname + ' neighbor ' + groupipv6 + ' export ' + groupname + '-ipv6-outbound'+ '\n'
-    commands += 'set protocols bgp group ' + groupname + ' neighbor ' + groupipv6 + ' peer-as ' + str(ix['asn']) + '\n'
-    commands += 'set protocols bgp group ' + groupname + ' neighbor ' + groupipv6 + ' description "' + ASNdesc + '"\n'
-    commands += 'set protocols bgp group ' + groupname + ' neighbor ' + groupipv6 + ' family inet6 unicast' + '\n'
+    f = open('template.j2','r')
+    groupFilename = groupname + '-AS' + str(ix['asn']) + '.set'
+    groupFile = open(groupFilename,'w+')
+    template = Template(f.read()).render(groupname=groupname,groupipv4=groupipv4,groupipv6=groupipv6,ASNdesc=ASNdesc)
+    commands += template
+    groupFile.write(commands)
+    groupFile.close()
+    print('Commands written to file ' + groupFilename + '!')
 
-print(commands)
-f = open('bgp_config.txt','w+')
+#print(commands)
+f = open('config_full.set','w+')
 f.write(commands)
 f.close()
-print('Commands written to file bgp_config.txt!')
+print('Commands written to file config_full.set!')
+userInput = input('Would you like to view the full commands? ')
+if userInput == 'y' or userInput == 'Y': print(commands)
